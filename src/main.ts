e44ad7add703f92dae26971eedbb80a2af5cb009
Forceres <ilya.sereda.2001@gmail.com>
2023-03-29 00:50:58 +0300

    Swagger connect, PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
+import { PORT } from './environments/env';
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+
+  const configuration = new DocumentBuilder()
+    .setTitle('TODO List')
+    .setDescription('Documentation')
+    .addTag('forceres')
+    .build();
+  const documentation = SwaggerModule.createDocument(app, configuration);
+  SwaggerModule.setup('/api/doc', app, documentation);
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 }
-bootstrap();
+
+start();
