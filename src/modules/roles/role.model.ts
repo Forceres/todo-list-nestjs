d62a4d292d26148ae0bf6bc24195edf2081dc15d
Forceres <ilya.sereda.2001@gmail.com>
2023-03-29 21:16:28 +0300

    The realisation of ROLE ENTITY (Model)

--- src/modules/roles/role.model.ts
+++ src/modules/roles/role.model.ts
@@ -1,5 +1,30 @@
-import { Model, Table } from "sequelize-typescript";
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
+interface RoleCreationAttributes {
+  title: string;
+  description: string;
+}
 
-@Table({tableName: 'role'})
-export class Role extends Model<Role>{}
+@Table({ tableName: 'role', updatedAt: false, createdAt: false })
+export class Role extends Model<Role, RoleCreationAttributes> {
+  @ApiProperty({example: '1c6e6134-7d4a-4b39-8ca4-4d53fddf16f2', description: 'UUIDV4 as RoleId (PK)'})
+  @Column({ type: DataType.UUID, unique: true, primaryKey: true, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @ApiProperty({example: 'ADMIN', description: 'Name of Role'})
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  title: string;
+
+  @ApiProperty({example: 'Administator with full rights', description: 'Short information of Role'})
+  @Column({
+    type: DataType.STRING,
+    unique: false,
+    allowNull: false,
+  })
+  description: string;
+}
