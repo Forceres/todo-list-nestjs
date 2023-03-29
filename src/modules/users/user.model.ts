5f55b7734dc52d6aa3d27837227b20352be63f06
Forceres <forceres31@gmail.com>
2023-03-29 21:15:07 +0300

    Try to make Hook

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,4 +1,5 @@
-import { Column, CreatedAt, DataType, Is, Model, Table, UpdatedAt } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import { AfterUpdate, Column, CreatedAt, DataType, Model, Table, UpdatedAt } from 'sequelize-typescript';
 
 interface UserCreationAttributes {
   username: string;
@@ -7,6 +8,8 @@ interface UserCreationAttributes {
 
 @Table({ tableName: 'user' })
 export class User extends Model<User, UserCreationAttributes> {
+
+  @ApiProperty({example: '1c6e6134-7d4a-4b39-8ca4-4d53fddf16f2', description: 'UUIDV4 as UserId (PK)'})
   @Column({ type: DataType.UUID, unique: true, primaryKey: true, defaultValue: DataType.UUIDV4 })
   id: string;
   @Column({
@@ -20,6 +23,8 @@ export class User extends Model<User, UserCreationAttributes> {
   //     throw new Error("Password must be from 8 chars to 30 chars without any special chars, apart from underscore");
   //   }
   // })
+
+  @ApiProperty({example: 'qwerty123', description: 'Password of User'})
   @Column({
     type: DataType.STRING,
     unique: false,
@@ -33,8 +38,17 @@ export class User extends Model<User, UserCreationAttributes> {
     // },
   })
   password: string;
+
+  @ApiProperty({example: '15', description: 'The quantity of user tasks'})
   @Column({ type: DataType.INTEGER, unique: false, defaultValue: 0 })
   tasks_quantity: number;
+
+  // @AfterUpdate
+  // static afterUpdate(instance: User){
+  //   instance.updatedAt = new Date();
+  // }
+
+  @ApiProperty({example: '01/02/2000, 15:23:11', description: 'Date and time of creation'})
   @CreatedAt
   @Column({
     type: DataType.DATE,
@@ -53,6 +67,8 @@ export class User extends Model<User, UserCreationAttributes> {
     defaultValue: DataType.NOW,
   })
   createdAt: Date;
+
+  @ApiProperty({example: '01/02/2000, 15:23:11', description: 'Date and time of update'})
   @UpdatedAt
   @Column({
     type: DataType.DATE,
