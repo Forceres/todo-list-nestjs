10ca42edb60202737a9d62a38dac40d02a53c5e5
Forceres <ilya.sereda.2001@gmail.com>
2023-03-29 14:06:19 +0300

    Connection of UserModule, connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_PASSWORD,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+} from './environments/env';
+import { User } from './modules/users/user.model';
+import { UserModule } from './modules/users/user.module';
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: POSTGRES_HOST,
+      port: POSTGRES_PORT,
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
