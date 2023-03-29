277f4a6f6fda76d0801fbacbbb46ed344711e631
Forceres <ilya.sereda.2001@gmail.com>
2023-03-29 14:16:40 +0300

    Pattern for Role Entity and connection to it at AppModule

---
+++ src/modules/roles/role.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { RoleController } from "./role.controller";
+import { Role } from "./role.model";
+import { RoleService } from "./role.service";
+
+
+@Module({
+    controllers: [RoleController],
+    providers: [RoleService],
+    imports: [SequelizeModule.forFeature([Role])],
+    exports: [RoleService]
+})
+export class RoleModule {}
