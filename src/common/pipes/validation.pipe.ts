bae35aea379d23acaa43757af5ac4b86a27ac53b
Ilya <ilya.sereda.2004@mail.ru>
2023-04-03 01:11:59 +0300

    Correcting logic of validation pipe

--- src/common/pipes/validation.pipe.ts
+++ src/common/pipes/validation.pipe.ts
@@ -1,4 +1,4 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
 import { ValidationException } from '../exceptions/validation.exception';
@@ -6,7 +6,7 @@ import { ValidationException } from '../exceptions/validation.exception';
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
-    const object = plainToClass(metadata.metatype, value);
+    const object = await plainToClass(metadata.metatype, value);
     const errs = await validate(object);
     if (errs.length > 0) {
       const reasons = errs.map((err) => `${err.property}`);
