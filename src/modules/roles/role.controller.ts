04c0a6bd7d13e3ec15a96ed94bb75c47a5fd9605
Forceres <ilya.sereda.2001@gmail.com>
2023-04-03 16:26:41 +0300

    The realisation of RoleModule; Bond: One role has many users

--- src/modules/roles/role.controller.ts
+++ src/modules/roles/role.controller.ts
@@ -1,8 +1,37 @@
-import { Controller } from "@nestjs/common";
-import { RoleService } from "./role.service";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateRoleDto } from './dto/create.role.dto';
+import { RoleService } from './role.service';
+import { Role } from './role.model';
 
+@ApiTags('Roles')
+@Controller('roles')
+export class RoleController {
+  constructor(private roleService: RoleService) {}
 
-@Controller('role')
-export class RoleController{
-    constructor(private roleService: RoleService ) {}
+  @ApiOperation({ summary: 'Role creating' })
+  @ApiResponse({ status: HttpStatus.OK, type: Role })
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createRole(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.createRole(createRoleDto);
+  }
+
+  @ApiOperation({ summary: 'Getting the role by its title' })
+  @ApiResponse({ status: HttpStatus.OK, type: Role })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, type: NotFoundException })
+  @Get(':title')
+  async getRoleByTitle(@Param('title') title: string) {
+    return this.roleService.getRoleByTitle(title);
+  }
 }
