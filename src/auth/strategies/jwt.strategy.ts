6f65caf6ad5b2c17acb3f3392c88c7f54949252d
Forceres <ilya.sereda.2001@gmail.com>
2023-04-03 16:48:06 +0300

    The realisation of LocalStrategy, JwtStrategy; AuthService: adding private token info from environment, connecting UserService, signIn func, signUp func, connecting JwtService, bcrypt; AuthModule: JwtModule registration, connecting private token info, LocalStrategy, JwtStrategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { SECRET_KEY } from 'src/environments/env';
+import { Role } from 'src/modules/roles/role.model';
+import { UserService } from 'src/modules/users/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: SECRET_KEY,
+    });
+  }
+
+  async validate(payload: { sub: string; username: string; role: Role }) {
+    await this.userService.getUserByUsername(payload.username);
+    return { id: payload.sub, username: payload.username, role: payload.role };
+  }
+}
