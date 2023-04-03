e5e85065f153e781af5d43eee989ca1b0a645ef9
Forceres <ilya.sereda.2001@gmail.com>
2023-04-03 16:26:41 +0300

    The realisation of RoleModule; Bond: One role has many users

--- src/modules/roles/role.service.ts
+++ src/modules/roles/role.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/sequelize";
-import { Role } from "./role.model";
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateRoleDto } from './dto/create.role.dto';
+import { Role } from './role.model';
 
 @Injectable()
-export class RoleService{
-    constructor(@InjectModel(Role) roleRepository: typeof Role) {}
-    
+export class RoleService {
+  constructor(@InjectModel(Role) private roleRepository: typeof Role) {}
+
+  async createRole(dto: CreateRoleDto) {
+    const role = this.roleRepository.create(dto);
+    return role;
+  }
+
+  async getRoleByTitle(title: string) {
+    const role = this.roleRepository.findOne({ where: { title } });
+    if (!role) throw new NotFoundException('There is not such a role!');
+    return role;
+  }
 }
