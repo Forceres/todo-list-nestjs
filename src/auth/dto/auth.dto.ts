34f39be0cb74fed8d825c4a453f02065952b8ac5
Ilya <ilya.sereda.2004@mail.ru>
2023-04-03 16:37:09 +0300

    Creating AuthDto for internal purposes

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, Length } from 'class-validator';
+
+export class AuthDto {
+  @ApiProperty({ example: 'username98', description: 'Nickname P.S. Login' })
+  @IsNotEmpty({ message: 'This attribute cannot be empty!' })
+  @IsString({ message: 'It must be string type!' })
+  @Length(8, 25, { message: 'Username must be between 8 and 25 chars' })
+  readonly username: string;
+
+  @ApiProperty({ example: 'qwerty123', description: "User's password" })
+  @IsNotEmpty({ message: 'This attribute cannot be empty!' })
+  @IsString({ message: 'It must be string type!' })
+  @Length(8, 30, { message: 'Password must be between 8 and 30 chars' })
+  readonly password: string;
+}
