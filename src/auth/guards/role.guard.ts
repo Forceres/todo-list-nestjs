e19be2494f429269332c306f08441adb7ffbac6f
Forceres <ilya.sereda.2001@gmail.com>
2023-04-06 15:00:48 +0300

    RoleGuard realisation (version 1)

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { SECRET_KEY } from 'src/environments/env';
+
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride('role', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) return true;
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+    const jwtToken = authHeader.split(' ')[1];
+    const user = await this.jwtService.verifyAsync(jwtToken, {
+      secret: SECRET_KEY,
+    });
+    request.user = user;
+    return requiredRoles.includes(user.role);
+  }
+}
