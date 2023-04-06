38012460a9c76e500b353b3c775b942a2983c889
Forceres <ilya.sereda.2001@gmail.com>
2023-04-06 15:09:33 +0300

    UserModule: forwardRef(() => Module) to avoid circular dependencies, connecting AuthModule; UserController: prettier

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize/dist';
 import { RoleModule } from '../roles/role.module';
 import { UserController } from './user.controller';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [SequelizeModule.forFeature([User]), RoleModule],
+  imports: [
+    SequelizeModule.forFeature([User]),
+    RoleModule,
+    forwardRef(() => AuthModule),
+  ],
   exports: [UserService],
 })
 export class UserModule {}
