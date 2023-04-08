64b5de528601cac381efab4cae578f2e074f0161
Ilya <ilya.sereda.2004@mail.ru>
2023-04-08 13:24:44 +0300

    Creating ListModule, ListService, ListController, ListModel with initial functionality, creating Dto for list

---
+++ src/modules/lists/dto/create.list.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateListDto {
+  @ApiProperty({
+    example: 'Work List',
+    description: 'Short title for your List of Tasks',
+  })
+  @IsNotEmpty({ message: 'This attribute cannot be empty!' })
+  @IsString({ message: 'It must be string type!' })
+  @Length(10, 40, { message: 'Title must be between 10 and 40 chars' })
+  title: string;
+}
