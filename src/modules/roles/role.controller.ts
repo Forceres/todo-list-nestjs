cb0bc70c5d32d4fa2821056c62c10fc885d0b2ca
Forceres <ilya.sereda.2001@gmail.com>
2023-04-12 03:39:37 +0300

    RoleModule: connecting JwtModule; RoleController: attaching RoleGuard and RolesDecorator, deleting commented code; RoleService: Refactoring

--- src/modules/roles/role.controller.ts
+++ src/modules/roles/role.controller.ts
@@ -5,32 +5,28 @@ import {
   HttpStatus,
   NotFoundException,
   Param,
-  Post,
-  UsePipes,
-  ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { Role } from './role.model';
 import { RoleService } from './role.service';
-import { CreateRoleDto } from './dto/create.role.dto';
+
+import { JwtAuthGuard } from '../../auth/guards/jwt.auth.guard';
+import { RoleGuard } from '../../auth/guards/role.guard';
+import { Roles } from './role.decorator';
 
 @ApiTags('Roles')
 @Controller('roles')
 export class RoleController {
   constructor(private roleService: RoleService) {}
 
-  @ApiOperation({ summary: 'Role creating' })
-  @ApiResponse({ status: HttpStatus.OK, type: Role })
-  @UsePipes(ValidationPipe)
-  @Post()
-  async createRole(@Body() createRoleDto: CreateRoleDto) {
-    return this.roleService.createRole(createRoleDto);
-  }
-
   @ApiOperation({ summary: 'Getting the role by its title' })
   @ApiResponse({ status: HttpStatus.OK, type: Role })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, type: NotFoundException })
+  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN', 'MODERATOR')
+  @UseGuards(RoleGuard)
   @Get(':title')
   async getRoleByTitle(@Param('title') title: string) {
     return this.roleService.getRoleByTitle(title);
