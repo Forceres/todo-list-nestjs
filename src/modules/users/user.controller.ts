3546236b43afb06beeef12e014603aea0261fbf1
Ilya <ilya.sereda.2004@mail.ru>
2023-04-12 03:43:28 +0300

    UserService: the implementation of updateUserRole method; UserController: creating Put request in order to update the role of the user, deleting commented code, attaching RoleDecorator and RoleGuard

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -15,29 +15,25 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { User } from './user.model';
 import { UserService } from './user.service';
-import { UpdateUserDto } from './dto/update.user.dto';
 
-import { ValidationException } from '../../common/exceptions/validation.exception';
 import { JwtAuthGuard } from '../../auth/guards/jwt.auth.guard';
+import { ValidationException } from '../../common/exceptions/validation.exception';
+
+import { Roles } from '../roles/role.decorator';
+import { RoleGuard } from '../../auth/guards/role.guard';
+import { UpdateRoleDto } from '../roles/dto/update.role.dto';
+
 
 @ApiTags('Users')
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  // @ApiOperation({ summary: 'User creation' })
-  // @ApiResponse({ status: HttpStatus.OK, type: User })
-  // @ApiResponse({ status: HttpStatus.FORBIDDEN, type: ForbiddenException })
-  // @ApiResponse({ status: HttpStatus.BAD_REQUEST, type: ValidationException })
-  // @UsePipes(ValidationPipe)
-  // @Post()
-  // async create(@Body() userDto: CreateUserDto): Promise<User> {
-  //   return await this.userService.createUser(userDto);
-  // }
-
   @ApiOperation({ summary: 'Get all users' })
   @ApiResponse({ status: HttpStatus.OK, type: [User] })
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN', 'MODERATOR')
+  @UseGuards(RoleGuard)
   @Get()
   async getAll(): Promise<User[]> {
     return await this.userService.getAllUsers();
@@ -47,29 +43,32 @@ export class UserController {
   @ApiResponse({ status: HttpStatus.OK, type: User })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, type: NotFoundException })
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN', 'MODERATOR')
+  @UseGuards(RoleGuard)
   @Get(':id')
   async getById(@Param('id') id: string): Promise<User> {
     return await this.userService.getUserById(id);
   }
 
-  @ApiOperation({ summary: "Update user's password" })
-  @ApiResponse({ status: HttpStatus.CREATED, type: User })
+  @ApiOperation({ summary: 'Change the role of the user' })
+  @ApiResponse({ status: HttpStatus.OK, type: User })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, type: NotFoundException })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, type: ValidationException })
   @UsePipes(ValidationPipe)
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RoleGuard)
   @Put(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() updateUserDto: UpdateUserDto
-  ): Promise<User> {
-    return await this.userService.updatePassword(id, updateUserDto);
+  async updateRole(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto): Promise<User> {
+    return await this.userService.updateUserRole(id, updateRoleDto);
   }
 
   @ApiOperation({ summary: 'Delete user' })
   @ApiResponse({ status: HttpStatus.OK, type: Promise<void> })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, type: NotFoundException })
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RoleGuard)
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
     return this.userService.removeUser(id);
