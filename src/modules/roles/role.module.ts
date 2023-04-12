893c8be49dcc41e7adfb28f550bd7a48a3b0e777
Ilya <ilya.sereda.2004@mail.ru>
2023-04-12 03:39:37 +0300

    RoleModule: connecting JwtModule; RoleController: attaching RoleGuard and RolesDecorator, deleting commented code; RoleService: Refactoring

--- src/modules/roles/role.module.ts
+++ src/modules/roles/role.module.ts
@@ -4,11 +4,15 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from './role.model';
 import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [RoleController],
   providers: [RoleService],
-  imports: [SequelizeModule.forFeature([Role])],
+  imports: [
+    SequelizeModule.forFeature([Role]),
+    JwtModule
+  ],
   exports: [RoleService],
 })
 export class RoleModule {}
