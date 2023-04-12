c08845fe50a260ea44da58166cf7668f80f120dd
Forceres <ilya.sereda.2001@gmail.com>
2023-04-12 04:07:03 +0300

    Fixing up the bug (user.role -> user.role.title); Adding try&catch unit to detect requests without JwtToken

--- src/auth/guards/role.guard.ts
+++ src/auth/guards/role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 
@@ -9,18 +9,25 @@ export class RoleGuard implements CanActivate {
   constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride('role', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) return true;
-    const request = context.switchToHttp().getRequest();
-    const authHeader = request.headers.authorization;
-    const jwtToken = authHeader.split(' ')[1];
-    const user = await this.jwtService.verifyAsync(jwtToken, {
-      secret: SECRET_KEY,
-    });
-    request.user = user;
-    return requiredRoles.includes(user.role);
+    try
+    {
+      const requiredRoles = this.reflector.getAllAndOverride('role', [
+        context.getHandler(),
+        context.getClass(),
+      ]);
+      if (!requiredRoles) return true;
+      const request = context.switchToHttp().getRequest();
+      const authHeader = request.headers.authorization;
+      const jwtToken = authHeader.split(' ')[1];
+      const user = await this.jwtService.verifyAsync(jwtToken, {
+        secret: SECRET_KEY,
+      });
+      request.user = user;
+      return await requiredRoles.includes(user.role.title);
+    }
+    catch (err) {
+      throw new ForbiddenException('Unauthorized!')
+    }
+
   }
 }
