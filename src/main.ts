a87825e6254809b1aed4a5271c0805310b1e220e
Forceres <ilya.sereda.2001@gmail.com>
2023-04-19 22:43:48 +0300

    AppModule: connecting TaskModule, LoggerModule, MorganMiddleware, LoggerService, establishing log options for sql requests (sequelize); main.ts: Establishing LoggerService instance and buggerLogs option, solving swagger issues, wrapping our code with a try-catch unit to process InternalServerErrorException, creating events to process unhandledRejections and uncaughtExceptions

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,62 @@
 import { NestFactory } from '@nestjs/core';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { DocumentBuilder } from '@nestjs/swagger';
+
 import { SwaggerModule } from '@nestjs/swagger/dist';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 import { PORT } from './environments/env';
+import { LoggerService } from './common/logger/logger.service';
 
 async function start() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  try {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      bufferLogs: true,
+      logger: new LoggerService(),
+    });
 
-  const configuration = new DocumentBuilder()
-    .setTitle('TODO List')
-    .setDescription('Documentation')
-    .addTag('forceres')
-    .build();
-  const documentation = SwaggerModule.createDocument(app, configuration);
-  SwaggerModule.setup('/api/doc', app, documentation);
+    const configuration = new DocumentBuilder()
+      .setTitle('TODO List')
+      .setDescription('Documentation')
+      .setContact(
+        'forceres',
+        'https://github.com/Forceres',
+        'ilya.sereda.2001@gmail.com'
+      )
+      .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        },
+        'JWT-auth'
+      )
+      .build();
+    const documentation = SwaggerModule.createDocument(app, configuration);
+    SwaggerModule.setup('/api/doc', app, documentation);
 
-  await app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-  });
+    await app.listen(PORT, () => {
+      Logger.log(`Server started on PORT: ${PORT}`);
+    });
+
+    process.on('unhandledRejection', (err) => {
+      Logger.error(err);
+    });
+
+    process.on('uncaughtException', (err) => {
+      Logger.error(err);
+    });
+  } catch (error) {
+    Logger.error(`while starting server, ${error}`, '', 'Start', false);
+    throw new InternalServerErrorException('Internal Server Error', error);
+  }
 }
 
-start();
+start().catch(() => {
+  process.exit(1);
+});
