7d3c002963d17092fe1f2bdc49b626e1054df0cb
Forceres <forceres31@gmail.com>
2023-04-23 20:19:08 +0300

    Deleting console.log(s)

--- test/globalTeardown.ts
+++ test/globalTeardown.ts
@@ -2,17 +2,14 @@ import { req, mockUserId } from './setupTest';
 import { getAdminToken, removeUserByAdmin } from './utils';
 
 const signOut = async () => {
-  console.log(mockUserId);
   if (mockUserId) {
     const headersAdmin = { Accept: 'application/json ' };
     const result = await getAdminToken(req);
     headersAdmin['Authorization'] = result.token;
     await removeUserByAdmin(req, mockUserId, headersAdmin);
   }
-  console.log('I have finished my task!');
 };
 
 export default async function globalTeardown() {
-  console.log('Started');
   await signOut();
 }
